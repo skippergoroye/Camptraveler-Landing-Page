3456fdcf2b78ef4ec02ff75340a814a642075efd
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-31 14:04:29 +0100

    feat: check here

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -5,7 +5,9 @@ import Button from "@/components/Button";
 
 const Navbar = () => {
   return (
+   
     <nav className="flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 2xl:px-0 relative z-30 py-5">
+       <h1>jfjjfjf</h1>
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
