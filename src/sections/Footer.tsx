0566368242e436d593fec32710222ac9ff0609a4
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-31 14:25:43 +0100

    fix: missing key index fixed on footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,11 +3,7 @@ import { FOOTER_LINKS, FOOTER_CONTACT_INFO, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
-
-
 const Footer = () => {
-
-
   return (
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
@@ -17,8 +13,8 @@ const Footer = () => {
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn title={columns.title} key={index}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
                     <Link href="/" key={link}>
@@ -37,9 +33,7 @@ const Footer = () => {
                     key={link.label}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
-                    <p className="whitespace-nowrap">
-                      {link.label}:
-                    </p>
+                    <p className="whitespace-nowrap">{link.label}:</p>
                     <p className="medium-14 whitespace-nowrap text-blue-70">
                       {link.value}
                     </p>
@@ -51,8 +45,8 @@ const Footer = () => {
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {SOCIALS.links.map((link, index) => (
+                    <Link href="/" key={index}>
                       <Image src={link} alt="logo" width={24} height={24} />
                     </Link>
                   ))}
@@ -63,7 +57,9 @@ const Footer = () => {
         </div>
 
         <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
+        <p className="regular-14 w-full text-center text-gray-30">
+          2023 Hilink | All rights reserved
+        </p>
       </div>
     </footer>
   );
