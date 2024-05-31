c75166207b9998e05a4977bd2aefd920836c5ed7
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-31 14:33:02 +0100

    fix: nav error removed

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -7,7 +7,6 @@ const Navbar = () => {
   return (
    
     <nav className="flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 2xl:px-0 relative z-30 py-5">
-       <h1>jfjjfjf</h1>
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
